b45b9e334f81c80437ba4128309a303c67cc98bc
Wedlec129 <boryasokolov@gmail.com>
2023-08-15 12:23:56 +0300

    Initial Commit

---
+++ miniAppFoodsStore/miniAppFoodsStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniAppFoodsStoreApp.swift
+//  miniAppFoodsStore
+//
+//  Created by Борух Соколов on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct miniAppFoodsStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
